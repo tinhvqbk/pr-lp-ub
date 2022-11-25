952bb32621f4e2ea9e2da5a0fdcf96d62260dc08
Kyle(Tình Vũ) <tinhvqbk@gmail.com>
2022-11-25 17:57:55 +0700

    Create lp.js

---
+++ lp.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const colors = {
+  white: '#ffffff',
+  primary: '#00A0E3',
+  warning: '#ffc400'
+}
+
+const getEl = (id) => {
+  const element = document.querySelector('.js-' + id);
+  if (!element) {
+    throw new Error('Element not found: ' + id);
+  }
+  return element;
+};
+
+const tracking = (event, data) => {
+  if (window.gtag) {
+    {
+      window.gtag('event', event, {
+        ...data,
+        version: configs.version,
+      });
+    }
+  }
+};
